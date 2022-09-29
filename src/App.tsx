2ba6f1fe415bd4eaa63986d7498ae49adfa2d167
Shahid-cloud-hub <shahid@webevis.com>
2022-09-29 15:40:25 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
+import Layout from "./components/Layout/Layout";
+import ConnectWallet from "./components/ConnectWallet/ConnectWallet";
+import indexTheme from "./Theme/indexTheme";
+import AccountModal from "./components/AccountDetails/AccountModal";
+import "@fontsource/inter";
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ChakraProvider theme={indexTheme}>
+        <Layout>
+          <ConnectWallet handleOpenModal={onOpen} />
+          <AccountModal isOpen={isOpen} onClose={onClose} />
+        </Layout>
+      </ChakraProvider>
+    </>
   );
 }
 
